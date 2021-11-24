5b3781233a455786206151934ed5047686afeff5
nirajnagrale <41152282+nirajnagrale@users.noreply.github.com>
2021-11-25 01:43:43 +0530

    Add files via upload

---
+++ helper.mjs
@@ -0,0 +1,56 @@
+let barContainer = $('#barContainer');
+
+export function swap(arr, i, j) {
+    return new Promise((resolve, reject) => {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        let t = barContainer.children()[i].style.height;
+        barContainer.children()[i].style.height = barContainer.children()[j].style.height;
+        barContainer.children()[j].style.height = t;
+        // await pause(1000);
+        resolve();
+    });
+}
+export function barSwap(arr, i, j) {
+    return new Promise((resolve,reject)=>{
+        console.log(i,j);
+        let t = barContainer.children()[i].style.height;
+        barContainer.children()[i].style.height = barContainer.children()[j].style.height;
+        barContainer.children()[j].style.height = t;
+        resolve();
+    });
+}
+export function mark(arr, i) {
+    return new Promise((resolve, reject) => {
+        // console.log(barContainer.children());
+        barContainer.children()[i].style.backgroundColor = 'green';
+        // await pause();
+        resolve();
+    });
+}
+export function unmark(arr, i) {
+    console.log(i);
+    return new Promise((resolve, reject) => {
+        barContainer.children()[i].style.backgroundColor = 'red';
+        // await pause();
+        resolve();
+    });
+}
+export function done(arr, i) {
+    // console.log(i);
+    return new Promise((resolve, reject) => {
+        barContainer.children()[i].style.backgroundColor = 'grey';
+        // await pause();
+        resolve();
+    });
+}
+export function pause() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, 100);
+    });
+}
+
+
